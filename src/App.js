e32e7f3e9f5fc4787fcfed05f77b295391fcb485
juliamachin <juliaamachin@gmail.com>
2022-05-18 21:54:46 -0700

    icon added

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import { Route, Routes, Link } from 'react-router-dom';
 import Form from './Components/Form/Form';
 import MadLibz from './Components/MadLibz/MadLibz';
 
+
+
 function App() {
 
   return (
     <div>
       <nav>
         <Link to='/'>
-          <h2>MadLibs</h2>
+          <img src='/Madlibsicon 3.png' style={{width:250, height:100}} alt='MadLibs Icon' />
         </Link>
       </nav>
       <main>
