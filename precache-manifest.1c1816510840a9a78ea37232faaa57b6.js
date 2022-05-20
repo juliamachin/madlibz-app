384ed3bd90fd0a6425bae6d7fe9c5ffaba762ec7
juliamachin <juliaamachin@gmail.com>
2022-05-20 10:01:24 -0700

    Updates

---
+++ precache-manifest.1c1816510840a9a78ea37232faaa57b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "30f3ebe480776b564413",
+    "url": "/juliamachin/madlibz-app.git/static/css/main.ae69be1c.chunk.css"
+  },
+  {
+    "revision": "30f3ebe480776b564413",
+    "url": "/juliamachin/madlibz-app.git/static/js/main.095552c5.chunk.js"
+  },
+  {
+    "revision": "7dbab17f0542d1e18965",
+    "url": "/juliamachin/madlibz-app.git/static/js/runtime~main.3b752783.js"
+  },
+  {
+    "revision": "98d3cebafe85464b06a7",
+    "url": "/juliamachin/madlibz-app.git/static/js/2.c212d1f8.chunk.js"
+  },
+  {
+    "revision": "0dfa6c7ed0c2ffa41b95bee58100afc0",
+    "url": "/juliamachin/madlibz-app.git/index.html"
+  }
+];
