9a00ef248b65120841f73986faa20831bb2c39da
juliamachin <juliaamachin@gmail.com>
2022-05-20 10:05:11 -0700

    deploying....

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.js";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 
 // anytime a component wraps around another component...its called
 // as Higher Order Component
