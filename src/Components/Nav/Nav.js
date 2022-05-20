17c54f08cc312839ef63bc6e05f0aac0056f1490
juliamachin <juliaamachin@gmail.com>
2022-05-20 12:32:24 -0700

    image paths updated

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -8,7 +8,7 @@ const Nav = () => {
       <nav>
         <Link to="/">
           <img
-            src="/Madlibsicon 3.png"
+            src="./Madlibsicon 3.png"
             style={{ width: 250, height: 100 }}
             alt="MadLibs Icon"
           />
